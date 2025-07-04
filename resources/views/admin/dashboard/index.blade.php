868746d050a761e091f7b1daa442f5b83e4d6123
sergeemmanuelk <serge_em@outlook.fr>
2025-07-04 13:44:10 +0000

    chore: remove comment from index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,25 +1,3 @@
-{{-- <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form method="POST" action="{{ route('admin.logout') }}">
-                @csrf
-                <button onclick="event.preventDefault(); this.closest('form').submit();"
-                    style="background: green; color: white; padding: 10px;">Logout</button>
-            </form>
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout> --}}
-
 @extends('admin.layouts.master')
 
 @section('content')
